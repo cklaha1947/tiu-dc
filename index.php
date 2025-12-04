d5be5a9de0fb5299df6090c7e8fed3925e9942af
Chanchal Kumar Laha <136878644+cklaha1947@users.noreply.github.com>
2025-12-04 13:14:42 +0530

    Update header text in index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Chanchal Kumar Laha new 106448 </h1>
+    <h1>Chanchal Kumar Laha new 106448 newly updated </h1>
     <h1>Successfully run devops CI/CD Pipeline.</h1>
     <img style="width: 300px;height:auto;" src="./img/img1.jpeg" alt="">
 </body>
